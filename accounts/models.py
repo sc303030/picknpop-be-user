c6a583d1e229db71a0acdc6a75af429ab6b58826
이경현 <lkh6035@naver.com>
2024-06-05 14:59:58 +0900

    [feat] 240605 start app accounts

---
+++ accounts/models.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.shortcuts import resolve_url
+
+
+class User(AbstractUser):
+    nickname = models.CharField(max_length=20, unique=True)
+    avatar = models.ImageField(
+        blank=True,
+        upload_to="accounts/avatar/%Y/%m/%d",
+        help_text="48px * 48px 크기의 png/jpg 파일을 업로드해주세요.",
+    )
+
+    @property
+    def name(self):
+        return f"{self.first_name} {self.last_name}".strip()
+
+    @property
+    def avatar_url(self):
+        if self.avatar:
+            return self.avatar.url
+        else:
+            return resolve_url("pydenticon_image", self.username)
+
+    def __str__(self):
+        return f"{self.nickname}"
--- picknpop_be_user/settings.py
+++ picknpop_be_user/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # apps
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -101,6 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "accounts.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
