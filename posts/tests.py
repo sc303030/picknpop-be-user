03f252e914cb016edd6daed4c890fa7717843219
이경현 <lkh6035@naver.com>
2024-06-24 20:42:23 +0900

    [feat] 240624 post 생성 테스트 코드 추가

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+# tests.py
 
-# Create your tests here.
+import pytest
+from django.contrib.auth import get_user_model
+
+from .models import Post
+
+User = get_user_model()
+
+
+@pytest.fixture
+def user():
+    return User.objects.create_user(email="testuser@test.com", password="testpassword")
+
+
+@pytest.mark.django_db
+def test_create_post(user):
+    post_data = {"title": "Test Title", "content": "Test Content", "owner": user}
+    post = Post.objects.create(**post_data)
+
+    assert Post.objects.count() == 1
+    assert Post.objects.first().title == "Test Title"
+    assert Post.objects.first().content == "Test Content"
+    assert Post.objects.first().owner == user
