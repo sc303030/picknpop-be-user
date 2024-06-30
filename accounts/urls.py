f5e86598c540d5aaa03777eb9c7cb8202f019301
이경현 <lkh6035@naver.com>
2024-06-30 22:40:36 +0900

    [feat] 240630 jwt 토큰 커스텀

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import SignUpViewSet
+from .views import SignUpViewSet, MyTokenObtainPairView
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
+    TokenVerifyView,
 )
 
 router = DefaultRouter()
 router.register(r"sign-up", SignUpViewSet, basename="sign-up")
 
 urlpatterns = [
-    path("sign-in/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("sign-in/", MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("", include(router.urls)),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
-
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
+from rest_framework import status
 from .serializers import SignUpSerializer
 
 User = get_user_model()
@@ -11,3 +14,14 @@ class SignUpViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = SignUpSerializer
     permission_classes = [AllowAny]
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        if response.status_code == 200:
+            refresh = response.data["refresh"]
+            access = response.data["access"]
+            response.data["refresh_token"] = refresh
+            response.data["access_token"] = access
+        return response
