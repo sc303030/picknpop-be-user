2793fbae930342d54f6a84abdd060d398318918e
이경현 <lkh6035@naver.com>
2024-07-03 20:57:16 +0900

    [feat] 240703 댓글 모델 추가

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.db import models
 
@@ -5,10 +6,29 @@ from django.db import models
 User = get_user_model()
 
 
-class Post(models.Model):
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Post(BaseModel):
     title = models.CharField(max_length=255)
     content = models.TextField()
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts"
+    )
 
     def __str__(self):
         return self.title
+
+
+class Comment(BaseModel):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    message = models.TextField()
+
+    class Meta:
+        ordering = ["-id"]
--- posts/tests.py
+++ posts/tests.py
@@ -2,8 +2,10 @@
 
 import pytest
 from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework.test import APIClient
 
-from .models import Post
+from .models import Post, Comment
 
 User = get_user_model()
 
@@ -11,6 +13,11 @@ User = get_user_model()
 @pytest.fixture
 def user():
     return User.objects.create_user(email="testuser@test.com", password="testpassword")
+
+
+@pytest.fixture
+def api_client():
+    return APIClient()
 
 
 @pytest.mark.django_db
@@ -22,3 +29,19 @@ def test_create_post(user):
     assert Post.objects.first().title == "Test Title"
     assert Post.objects.first().content == "Test Content"
     assert Post.objects.first().owner == user
+
+
+@pytest.mark.django_db
+def test_add_comment_to_post(api_client, user):
+    api_client.login(email="testuser@test.com", password="testpassword")
+
+    post = Post.objects.create(title="Test Post", content="Test Content", owner=user)
+
+    comment = Comment.objects.create(author=user, post=post, message="Test Comment")
+
+    assert Comment.objects.count() == 1
+    assert Comment.objects.get().message == "Test Comment"
+    assert Comment.objects.get().post == post
+    assert Comment.objects.get().author == user
+
+    api_client.logout()
